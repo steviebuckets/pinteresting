57a20a0d250c375653af67d7243640173a5c4714
Stevie Buckets <steviebuckets33@gmail.com>
2015-03-10 18:35:16 -0700

    modified pin.rb model

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,7 +1,16 @@
 class Pin < ActiveRecord::Base
 	belongs_to :user
   
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+  has_attached_file :image, 
+  :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  :storage => :s3,
+  :bucket => ENV ['MY_BUCKET_NAME']
+  :s3_credentials => {
+  					:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+            :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  },
+  :s3_permissions => 'private',
+  :url => ":s3_domain_url"
 	
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
