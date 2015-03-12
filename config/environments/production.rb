64b8cf7135c135dbc87ab2d7f4975e9886d41503
Stevie Buckets <steviebuckets33@gmail.com>
2015-03-11 20:25:40 -0700

    modified productio.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # Required for Heroku
   # Note to set this to your actual host
-  config.action_mailer.default_url_options = { :host => 'http://pinterestingbuckets.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
   
 
   # Sets Paperclip to upload images to Amazon S3
