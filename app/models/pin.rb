793654d31e70b6c90fcc919c379acd37c1f3dfcc
Stevie Buckets <steviebuckets33@gmail.com>
2015-03-12 09:32:35 -0700

    modified pin rb file

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,4 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+     has_attached_file :image, validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] } :styles => { :medium => "300x300>", :thumb => "100x100>" }
 end
