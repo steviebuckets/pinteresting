c94c79de321159a47a2a7fdfa05281b60ea741fe
Stevie Buckets <steviebuckets33@gmail.com>
2015-03-12 14:10:56 -0700

    modified pin rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,4 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, validates_attachment_content_type :pin_name, :content_type => /\Aimage/.*\Z/
+     has_attached_file :image, validates_attachment_content_type :file_name, :content_type => /\Aimage/.*\Z/
    end
