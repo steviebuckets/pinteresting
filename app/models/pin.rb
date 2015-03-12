5742dad5df92aa212ca4fde047de23f1f3eb8d8d
Stevie Buckets <steviebuckets33@gmail.com>
2015-03-12 14:30:06 -0700

    made more changes to pin rb

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,10 @@
 class Pin < ActiveRecord::Base
-     belongs_to :user
-     has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-   end
+  belongs_to :user
+  has_attached_file :image 
+  # Validate content type
+  validates_attachment_content_type :image, :content_type => /\Aimage/
+  # Validate filename
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+  # Explicitly do not validate
+  do_not_validate_attachment_file_type :image
+end
