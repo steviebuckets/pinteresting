5e014cf53f3d62688823129add8a1a754e7728a8
Stevie Buckets <steviebuckets33@gmail.com>
2015-03-13 14:11:41 -0700

    modified schema rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20150308031707) do
 
-  create_table "pins", force: :cascade do |t|
+  create_table "pins", force: true do |t|
     t.string   "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at"         
+    t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "image_file_name"
     t.string   "image_content_type"
@@ -26,13 +26,13 @@ ActiveRecord::Schema.define(version: 20150308031707) do
 
   add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
