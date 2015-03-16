3a63fed13f0c2329ef23adaefc16331e6fb31169
Stevie Buckets <steviebuckets33@gmail.com>
2015-03-15 19:04:03 -0700

    restored original pin rb file

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,6 @@ class Pin < ActiveRecord::Base
   validates_attachment_content_type :image, :content_type => /\Aimage/
   # Validate filename
   validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/]
+    # Explicitly do not validate
+  do_not_validate_attachment_file_type :image
 end
